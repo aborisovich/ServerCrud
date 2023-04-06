0a34796f5b01b2068eb109a3bfb329c62314f35d
Andrey Borisovich <andrey@borisovich.com>
2023-04-07 00:24:04 +0200

    Added CMake project base tree
    Created a base structure for the C++ cmake project.
    
    Signed-off-by: Andrey Borisovich <andrey@borisovich.com>

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: Apache License 2.0
+ *
+ * Copyright(c) 2023 Andrey Borisovich. All rights reserved.
+ *
+ * Author: Andrey Borisovich <andrey@borisovich.com>
+ */
+
+#include <ServerCrud/ServerCrud.h>
+
+int main()
+{
+	return 0;
+}
