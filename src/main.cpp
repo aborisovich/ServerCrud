b39c6c50314c89002b8a56920e20061b4512be8a
Andrey Borisovich <andrey@borisovich.com>
2023-04-11 19:33:19 +0200

    ServerCrud has termination handler
    ServerCrud manages system resources such as network sockets.
    It needs to provide a controlled way of termination to release those
    resources. This commit adds two termination handlers that control
    application exit behavior. Additionally the Windows console behavior
    is handled - when user terminates ServerCrud instance using
    console signals like CTRL+C, CTRL+BREAK or console exit.
    
    Signed-off-by: Andrey Borisovich <andrey@borisovich.com>

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,22 @@
  */
 
 #include <ServerCrud/ArgParser.hpp>
+#include <ServerCrud/TerminationHandler.hpp>
+#ifdef WIN32
+#include <ServerCrud/WindowsConsole.hpp>
+#endif
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
+#ifdef WIN32
+	// Configures ServerCrud to be used from the Windows console
+	SetConsoleCtrlHandler(server_crud::console_handler, TRUE);
+#endif
+	std::set_terminate(server_crud::on_terminate);
+	if(std::atexit(server_crud::on_exit) != 0)
+		throw std::runtime_error("Critical initialization error - failed to register exit handler\n");
+
 	server_crud::ArgParser parser;
 	try
 	{
